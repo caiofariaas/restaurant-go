3641821b3dc09ed224c13fe87730f52a56757aab
caiofariaas <caiofarias077@gmail.com>
2024-11-18 08:25:07 -0300

    :sparkles: feat: first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+
+	"os"
+	"github.com/gin-gonic/gin"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/routes"
+	"golang-restaurant-management/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
+
+)
+
+func main(){
+	port := os.Getenv("PORT")
+
+	if port == ""{
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+
+}
