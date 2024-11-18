07766dbf0ab88a5a94efedec84e857ecc31e8058
caiofariaas <caiofarias077@gmail.com>
2024-11-18 13:55:35 -0300

    :sparkles: feat: routes

---
+++ routes/invoiceRouter.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "golang-restaurant-management/controllers"
+)
+
+func InvoiceRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.GET("/invoices", controller.GetInvoices())
+	incomingRoutes.GET("/invoices/:invoices_id", controller.GetInvoice())
+	incomingRoutes.POST("/invoices", controller.CreateInvoice())
+	incomingRoutes.PATCH("/invoices/:invoices_id", controller.UpdateInvoice())
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,9 +1,8 @@
 package routes
 
 import (
-	controller "golang-restaurant-management/controllers"
-
 	"github.com/gin-gonic/gin"
+	controller "golang-restaurant-management/controllers"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine){
@@ -11,5 +10,4 @@ func UserRoutes(incomingRoutes *gin.Engine){
 	incomingRoutes.GET("/users/:user_id", controller.GetUser())
 	incomingRoutes.POST("/users/signup", controller.SignUp())
 	incomingRoutes.POST("/users/login", controller.Login())
-
 }
